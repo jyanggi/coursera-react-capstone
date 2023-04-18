c66df6c28b562b3637d9041da2abf0c02f18f8ce
unknown <jyanggi888@gmail.com>
2023-04-18 18:45:58 +0800

    add outside mobile nav listener

--- src/components/header/MobileNav.jsx
+++ src/components/header/MobileNav.jsx
@@ -1,6 +1,6 @@
 import logo from "./Logo.svg";
 import "./MobileNav.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MdClose } from "react-icons/md";
 import { FiMenu } from "react-icons/fi";
 const SECTIONS = [
@@ -12,6 +12,19 @@ const SECTIONS = [
 ];
 function MobileNav() {
   const [isHidden, setIsHidden] = useState(true);
+  const navRef = useRef();
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setIsHidden(true);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [navRef]);
   return (
     <header id="mobile-nav">
       <img className="logo" src={logo} alt="header logo" />
@@ -30,7 +43,7 @@ function MobileNav() {
         </button>
       </section>
 
-      <nav className={isHidden ? "hidden" : "show"}>
+      <nav ref={navRef} className={isHidden ? "hidden" : "show"}>
         <img src={logo} alt="header logo" />
         <ul>
           {SECTIONS.map((p) => (
